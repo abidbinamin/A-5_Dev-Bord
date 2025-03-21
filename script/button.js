bc13b7d44e42fc5b288c4015683479049c10de2e
abidbinamin <abidbinamin9@gmail.com>
2025-03-21 07:28:11 +0600

    Error fixed

--- script/button.js
+++ script/button.js
@@ -1,5 +1,66 @@
-document.getElementById('complete-btn').addEventListener('click', function() {
-    this.disabled = true;
-    this.classList.add('bg-gray-500', 'text-gray-500');
-    alert("Board Updated Successfully");
+document.addEventListener('DOMContentLoaded', function() {
+  const completeButtons = document.querySelectorAll('#complete-btn');
+  const taskCountElement = document.querySelector('#task-card p');
+  const taskAddElement = document.querySelector('#task-add');
+  const activityLog = document.getElementById('activity');
+
+  
+  completeButtons.forEach(button => {
+    button.addEventListener('click', function() {
+          const taskCard = this.closest('.bg-blue-50');
+      const taskName = taskCard.querySelector('h2').textContent;
+          const currentTime = new Date().toLocaleTimeString();
+
+          alert('Board updated successfully');
+
+          let currentTaskCount = parseInt(taskCountElement.textContent);
+          
+          if (currentTaskCount > 0) {
+              taskCountElement.textContent = currentTaskCount - 1;
+          }
+
+
+          let currentNavbarCount = parseInt(taskAddElement.textContent);
+          taskAddElement.textContent = currentNavbarCount + 1;
+
+
+          const activityMessage = document.createElement('p');
+          
+          activityMessage.textContent = `You have completed the task ${taskName} at ${currentTime}`;
+          activityLog.insertBefore(activityMessage, activityLog.lastElementChild);
+
+          this.disabled = true;
+          this.classList.remove('bg-blue-500', 'hover:bg-blue-600');
+          this.classList.add('bg-gray-400', 'cursor-not-allowed');
+
+          
+          checkAllCompleted();
+      });
+  });
+
+  function checkAllCompleted() {
+    const remainingButtons = document.querySelectorAll('#complete-btn:not([disabled])');
+      if (remainingButtons.length === 0) {
+          alert("Congrats!!! You have completed all the current tasks");
+      }
+  }
 });
+
+
+
+// clear history
+
+document.addEventListener('DOMContentLoaded', function() {
+    const clearHistoryButton = document.querySelector('#clear-history-btn');
+    const activityLog = document.getElementById('activity');
+  
+    if (clearHistoryButton && activityLog) {
+      clearHistoryButton.addEventListener('click', function() {
+        const activityMessages = activityLog.querySelectorAll('p');
+        activityMessages.forEach(message => {
+          message.remove();
+        });
+      });
+    }
+    }
+  );
--- script/discover.js
+++ script/discover.js
@@ -1,3 +1,16 @@
 document.getElementById("discover-card").addEventListener("click",function(){
     window.location.href = "./blogs.html";
 })
+
+document.getElementById("back-btn").addEventListener("click",function(){
+    window.location.href = "./index.html";
+})
+
+
+
+
+
+
+
+
+
